725dabf3fa26262d404b1a1d7fd15b6504e5efda
InfinityJaaR <javieralfaroromero@gmail.com>
2024-06-03 00:39:21 -0600

    cambios realizados

--- RRHH/migrations/0001_initial.py
+++ RRHH/migrations/0001_initial.py
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
                 ('horas_trabajadas', models.IntegerField()),
                 ('horas_extras_diurnas', models.IntegerField()),
                 ('horas_extras_nocturnas', models.IntegerField()),
-                ('mes_asistencia', models.DateField()),
+                ('mes_asistencia', models.CharField(choices=[('1', 'Enero'), ('2', 'Febrero'), ('3', 'Marzo'), ('4', 'Abril'), ('5', 'Mayo'), ('6', 'Junio'), ('7', 'Julio'), ('8', 'Agosto'), ('9', 'Septiembre'), ('10', 'Octubre'), ('11', 'Noviembre'), ('12', 'Diciembre')],default='1',max_length=2)),
                 ('carnet', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='asistencias', to='RRHH.empleado')),
             ],
         ),
