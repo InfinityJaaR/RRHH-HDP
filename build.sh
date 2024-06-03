f1428d1b65e275dcc6f4765664cfa25050529773
InfinityJaaR <javieralfaroromero@gmail.com>
2024-06-03 13:54:15 -0600

    realizando cambios

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3 install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
