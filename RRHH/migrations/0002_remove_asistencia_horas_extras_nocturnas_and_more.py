3d0f8efca1899a1b0abe2bc0b66e7a94ab094bf4
InfinityJaaR <javieralfaroromero@gmail.com>
2024-06-03 00:44:53 -0600

    cambios realizados

--- RRHH/migrations/0002_remove_asistencia_horas_extras_nocturnas_and_more.py
+++ RRHH/migrations/0002_remove_asistencia_horas_extras_nocturnas_and_more.py
@@ -27,6 +27,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='asistencia',
             name='mes_asistencia',
-            field=models.IntegerField(),
+            field=models.CharField(choices=[('1', 'Enero'), ('2', 'Febrero'), ('3', 'Marzo'), ('4', 'Abril'), ('5', 'Mayo'), ('6', 'Junio'), ('7', 'Julio'), ('8', 'Agosto'), ('9', 'Septiembre'), ('10', 'Octubre'), ('11', 'Noviembre'), ('12', 'Diciembre')],default='1',max_length=2),
         ),
     ]
