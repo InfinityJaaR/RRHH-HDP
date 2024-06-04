cb29a41841d773f11f4d106b23462197caecdfb2
InfinityJaaR <javieralfaroromero@gmail.com>
2024-06-04 17:49:23 -0600

    realizando cambios

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip3 install -r requirements.txt
 
-if [[ $CREATE_SUPERUSER]]:
-then
-  python manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER]]:
+# then
+#   python manage.py createsuperuser --no-input
+# fi
 
 # Convert static asset files
 python manage.py collectstatic --no-input
