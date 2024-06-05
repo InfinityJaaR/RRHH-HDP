1b1be491e39c2278ae999ee2601436167fa44f4c
InfinityJaaR <javieralfaroromero@gmail.com>
2024-06-05 14:35:52 -0600

    realizando cambios

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 # if [[ $CREATE_SUPERUSER]]:
 # then
